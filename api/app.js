6077de5284ab9a89f5643d7d1e83814ca5ecce48
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-10-25 15:10:57 +0000

    mongoDB configuration

--- api/app.js
+++ api/app.js
@@ -1,9 +1,47 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-}),
+/* ROUTE HANDLERS */
+
+/* LIST ROUTES */
+
+/**
+ *  GET /lists
+ *  Purpose: GEt all lists
+ */
+
+app.get('/lists', (req, res) => {
+    // return an array of all the lists in the database
+})
+
+/**
+ *  POST /lists
+ *  Purpose: Create a list
+ */
+
+app.get('/lists', (req, res) => {
+    // Create a new list and return the new list document back to the user (wich includes the id)
+    // The list information (fields) will be passed in via the JSON request body
+})
+
+/**
+ *  PATCH /lists/:id
+ *  Purpose: Update a specified list
+ */
+
+app.patch('/lists/:id', (req, res) => {
+    // update the specified list (list document with id in the URL) with the new values specified in the JSON body of the request
+})
+
+/**
+ *  DELETE /lists/:id
+ *  Purpose: Delete a specified list
+ */
+
+
+app.delete('/lists/:id' , (req, res) => {
+    // Delete the specified list (document with the id in the URL)df
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
