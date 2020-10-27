bad6a41ffb157339e28a18a6cb861f4ca9190e7e
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-10-27 18:46:53 +0000

    creating angular services  /  get method

---
+++ frontend/src/app/task.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+
+  createList(title: string) {
+    
+  }
+}
