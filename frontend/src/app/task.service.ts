7eee59ba70869d1078887238e909754a320e8a04
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-10-28 16:23:47 +0000

    createNewList request

--- frontend/src/app/task.service.ts
+++ frontend/src/app/task.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
+import { WebRequestService } from './web-request.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  constructor(private webReqService: WebRequestService) { }
 
   createList(title: string) {
-    
+    // we want send a web request creat a list
+    return this.webReqService.post('/list', { title });
   }
 }
