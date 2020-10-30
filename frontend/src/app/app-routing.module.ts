2bf1cc75c7422aa3dd92184bc36a31156d367941
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-10-30 16:00:33 +0000

    new-list component created  / creaList added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewListComponent } from './pages/new-list/new-list.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 
 const routes: Routes = [
-  { path: '', component: TaskViewComponent }
+  { path: '', component: TaskViewComponent },
+  { path: 'new-list', component: NewListComponent },
 ];
 
 @NgModule({
